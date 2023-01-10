e4cc95480029aaa0f7f69293c4b01123c457fdcd
Sudharani <Sudharani@LAPTOP-HSJQEM5R>
2023-01-10 12:14:26 +0530

    my first commit

---
+++ Jenkins1/src/main/java/Demo/Test1.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void m1()
+	{
+		Reporter.log("hello",true);
+	}
+
+}
---
+++ Jenkins1/src/main/java/Demo/Test2.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void m2()
+	{
+		Reporter.log("hi",true);
+	}
+
+}
